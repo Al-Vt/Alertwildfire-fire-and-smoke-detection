2f088c7d2b4ff7becff2ff6a98faec5bcc03ff7f
Al-Vt <axel.vilamot@gmail.com>
2026-06-12 06:39:32 +0200

    alerte correction

--- dags/inference_dag.py
+++ dags/inference_dag.py
@@ -6,12 +6,47 @@ import os
 import requests
 import boto3
 import logging
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+from email.mime.image import MIMEImage
+from PIL import Image, ImageDraw
+import io
+
 
 sys.path.insert(0, '/app/scraper')
 from database import DatabaseManager
 
 INFERENCE_API_URL = os.getenv("INFERENCE_API_URL")
 S3_BUCKET = os.getenv("S3_BUCKET_NAME")
+
+
+# Alert if wildfire detected
+def send_alert_email(camera_name, confidence, image_bytes, bbox):
+    # Take the image returned by the model
+    image = Image.open(io.BytesIO(image_bytes)).convert("RGB")
+    # and draw the bounding box
+    ImageDraw.Draw(image).rectangle(bbox, outline="red", width=5)
+
+    # converting the image to JPEG
+    output = io.BytesIO()
+    image.save(output, format="JPEG")
+
+    # building email
+    msg = MIMEMultipart()
+    msg["Subject"] = f"[ALERTE WILDFIRE] Camera {camera_name}"
+    msg["From"] = os.getenv("ALERT_EMAIL_SENDER")
+    msg["To"] = os.getenv("ALERT_EMAIL_RECEIVER")
+    # Texte with camera name and confidence score 
+    msg.attach(MIMEText(f"Fire detected on camera {camera_name} (confidence : {confidence:.2f})"))
+    # image returned by the model
+    msg.attach(MIMEImage(output.getvalue(), name=f"{camera_name}.jpg"))
+
+    # sent via Gmail
+    server = smtplib.SMTP_SSL("smtp.gmail.com", 465)
+    server.login(os.getenv("ALERT_EMAIL_SENDER"), os.getenv("ALERT_EMAIL_PASSWORD"))
+    server.sendmail(msg["From"], msg["To"], msg.as_string())
+    server.quit()
 
 
 def run_inference():
@@ -35,14 +70,16 @@ def run_inference():
                 f"{INFERENCE_API_URL}/predict",
                 files={"file": ("image.jpg", image_bytes)},
             )
-            detections = response.json().get("confiance", [])
+            detections = response.json().get("detections", [])
 
             # Detection 
             if detections:
                 fire_detected = True
+                # The first box you choose is the one with the highest level of trust
                 confidence = detections[0]["confidence"]
                 bbox = detections[0]["bbox"]
-                logging.info(f"[ALERT] wildfire detected ! image_id={image['id']}, camera={image['camera_name']}")
+                logging.info(f"[ALERT] wildfire detected, image_id={image['id']}, camera={image['camera_name']}")
+                send_alert_email(image["camera_name"], confidence, image_bytes, bbox)
             else:
                 fire_detected = False
                 confidence = 0.0
@@ -52,7 +89,6 @@ def run_inference():
             db.update_prediction(image["id"], fire_detected, confidence, bbox)
 
     db.close()
-
 
 
 default_args = {
